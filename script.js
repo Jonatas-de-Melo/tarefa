6e8a570f834efad0eb806027a43e8e8a3731d54c
Jonatas-de-Melo <84332883+Jonatas-de-Melo@users.noreply.github.com>
2023-05-15 21:37:39 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,39 @@
-//buscar as regerencias dos elementos html
+// Buscar as referências dos elementos HTML
+const taskInput = document.getElementById('task-input');
+const addButton = document.getElementById('add-button');
+const todoList = document.getElementById('todo-list');
 
-const taskInput = document.getElementById('taskInput');
-const addButton = document.getElementById('addButton');
-const todoList = document.getElementById('todoList');
+// Função para adicionar uma nova tarefa
+function addTask() {
+  // Retomar a tarefa digitada pelo usuário
+  const taskDescription = taskInput.value;
 
-//funçao para adicionar uma tarefa
+  // Criar um novo item de lista e adiciona-lo à lista
+  const listItem = document.createElement('li');
+  listItem.className = 'list-group-item';
+  listItem.innerHTML = `
+    <span>${taskDescription}</span>
+    <i class="fas fa-trash-alt trash-icon"></i>
+  `;
+  todoList.appendChild(listItem);
 
-function addTask(){
-    //retona o valor da tarefa digitada pelo usuario
+  // Limpar o campo de entrada de texto
+  taskInput.value = '';
 
-        const   taskDescription = taskiInput.value;
+  // Adicionar um event listener ao ícone de lixeira
+  const trashIcon = listItem.querySelector('.trash-icon');
+  trashIcon.addEventListener('click', function() {
+    listItem.remove();
+  });
+}
 
-        //criar um novo intem de lista e adicionar a lista
-        const listIntem = document.createElemmet('li');
-        listIntem.className = 'list-group-item';
-        listIntem.innerHtml = `<span>${taskDescription}</span>;`;
-        todoList.appendChild(listIntem);
+// Adicionar um event listener ao botão de adicionar
+addButton.addEventListener('click', addTask);
 
-        //limpar o campo de entrada de texto
-        taskInput.value = '';
-
-        //adicionar um evento de clique para remover o item da lista
-
-        const trashiIcon = listIntem.querySelector('.trash-icon');
-        trashiIcon.addEventListener('click', function(){
-            listIntem.remove();
-        });
-
-    }
+// Adicionar um event listener ao campo de entrada de texto quando apertar enter
+taskInput.addEventListener('keypress', function(event) {
+  if (event.key === 'Enter') {
+    event.preventDefault(); // Prevenir o comportamento padrão do enter
+    addTask(); // Chamar a função de adicionar tarefa
+  }
+});
